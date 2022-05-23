3e8342f8ef8d1ed42de8bb1994d09d3601525503
Julien Chagnon <jc.chagnon@gmail.com>
2022-05-23 15:53:05 +1000

    rails stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,16 @@
 class QuestionsController < ApplicationController
-  def ask
-  end
+
+   def ask
+   end
 
   def answer
+    @user_answer = params[:user_answer]
+    if @user_answer == "I am going to work"
+      @coach_reply = "Great!"
+    elsif @user_answer.end_with?("?")
+      @coach_reply = "Silly question, get dressed and go to work!"
+    else
+      @coach_reply = "I don't care, get dressed and go to work!"
+    end
   end
 end
